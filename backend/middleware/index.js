7136d934555aa0ecf3d3277e4511a423dd1328e8
gautamkandpal <gkandpal33@gmail.com>
2024-10-09 12:31:50 +0530

    chors: bug fixes

--- backend/middleware/index.js
+++ backend/middleware/index.js
@@ -1,6 +1,7 @@
 const { expressjwt } = require("express-jwt");
 const Hotel = require("../model/hotelsSchema");
 
+//req.user
 const requireSignin = expressjwt({
   secret: process.env.JWT_SECRET,
   algorithms: ["HS256"],
@@ -8,10 +9,7 @@ const requireSignin = expressjwt({
 
 const hotelOwner = async (req, res, next) => {
   let hotel = await Hotel.findById(req.params.hotelId);
-  //TODO: need to fix the id bug asap
-  // console.log("from req.params", req.params);
-  // console.log("from hotelOwner", res);
-  let owner = hotel.postedBy.id.toString() === req.auth._id.toString();
+  let owner = hotel.postedBy._id.toString() === req.auth._id.toString();
 
   if (!owner) {
     return res.status(403).send("Unauthorized");
